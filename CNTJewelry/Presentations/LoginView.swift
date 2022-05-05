7fdace27fb4967ab8eb947b99faec85b68d37b70
Neil Francis Hipona <neil.francis.hipona@codeandtheory.com>
2022-05-05 13:54:19 +0800

    Update color profiles

---
+++ CNTJewelry/Presentations/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  CNTJewelry
+//
+//  Created by Neil Francis Hipona on 5/5/22.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
---
+++ CNTJewelry/Presentations/LoginViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewModel.swift
+//  CNTJewelry
+//
+//  Created by Neil Francis Hipona on 5/5/22.
+//
+
+import Foundation
