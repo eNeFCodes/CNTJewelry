83aca45c30b4363947bfae605349c0d2ab47d63d
Neil Francis Hipona <neil.francis.hipona@codeandtheory.com>
2022-06-16 12:00:48 +0800

    Add bulleted text view

--- CNTJewelry/Presentations/ArticlePageView/ArticleDetailsView/ArticleTextView.swift
+++ CNTJewelry/Presentations/ArticlePageView/ArticleDetailsView/ArticleTextView.swift
@@ -21,12 +21,15 @@ struct ArticleTextView: View {
   var body: some View {
     VStack(spacing: 8) {
       let contentWidth = geometry.size.width - (padding * 2)
-      let titleFont = FontCollection.BrilliantCutProB7.medium(size: 14).font
-      Text(model.title)
-        .accessibilityLabel(model.title)
-        .font(titleFont)
-        .foregroundColor(ColorCollection.black)
-        .frame(width: contentWidth, alignment: .leading)
+
+      if !model.title.isEmpty {
+        let titleFont = FontCollection.BrilliantCutProB7.medium(size: 14).font
+        Text(model.title)
+          .accessibilityLabel(model.title)
+          .font(titleFont)
+          .foregroundColor(ColorCollection.black)
+          .frame(width: contentWidth, alignment: .leading)
+      }
 
       let detailsFont = FontCollection.FancyCutProB7.regular(size: 18).font
       Text(model.details)
@@ -34,9 +37,47 @@ struct ArticleTextView: View {
         .font(detailsFont)
         .foregroundColor(ColorCollection.black)
         .frame(width: contentWidth, alignment: .leading)
+
+      if let bullet = model.bullet {
+        buildBulletedTextListViewStack(contentWidth: contentWidth, bullet: bullet)
+          .padding(.top, 24)
+      }
     }
     .padding(.horizontal, padding)
     .frame(width: geometry.size.width, alignment: .leading)
+  }
+
+  private func buildBulletedTextListViewStack(contentWidth: CGFloat, bullet: ArticleTextViewModel.Bullet) -> some View {
+    VStack(spacing: 8) {
+      let textFont = FontCollection.FancyCutProB7.regular(size: 18).font
+      Text(bullet.title)
+        .accessibilityLabel(bullet.title)
+        .font(textFont)
+        .foregroundColor(ColorCollection.black)
+        .frame(width: contentWidth, alignment: .leading)
+
+      ForEach(bullet.bulletedText, id: \.self) { text in
+        HStack {
+          Circle()
+            .frame(width: 6, height: 6, alignment: .center)
+            .foregroundColor(ColorCollection.gold)
+
+          Text(text)
+            .accessibilityLabel(text)
+            .font(textFont)
+            .foregroundColor(ColorCollection.black)
+        }
+        .frame(width: contentWidth, alignment: .leading)
+      }
+
+      if !bullet.details.isEmpty {
+        Text(bullet.details)
+          .accessibilityLabel(bullet.details)
+          .font(textFont)
+          .foregroundColor(ColorCollection.black)
+          .frame(width: contentWidth, alignment: .leading)
+      }
+    }
   }
 }
 
@@ -44,7 +85,12 @@ struct ArticleTextView_Previews: PreviewProvider {
   static var previews: some View {
     GeometryReader { geometry in
       ArticleTextView(model: .init(title: "A favorite amongst artists".uppercased(),
-                                   details: "Cartier Stockholm was first international jeweler to open its doors in the city—and represents our first step into the Scandinavian market. Here’s everything you need to know about the boutique and its importance to the Maison."),
+                                   details: "Cartier Stockholm was first international jeweler to open its doors in the city—and represents our first step into the Scandinavian market. Here’s everything you need to know about the boutique and its importance to the Maison.",
+                                   bullet: .init(title: "The trio of gold holds a powerful metaphor:",
+                                                 bulletedText: ["Pink for love",
+                                                                "White for friendship",
+                                                                "Yellow for faithfulness"],
+                                                 details: "More than a symbol of love, the pureness of its lines also offers multiple interpretations.")),
                       geometry: geometry)
     }
   }
--- CNTJewelry/Presentations/ArticlePageView/ArticleDetailsView/ArticleTextViewModel.swift
+++ CNTJewelry/Presentations/ArticlePageView/ArticleDetailsView/ArticleTextViewModel.swift
@@ -7,12 +7,28 @@
 
 import Foundation
 
+extension ArticleTextViewModel {
+  struct Bullet {
+    let title: String
+    let bulletedText: [String]
+    let details: String
+
+    init(title: String, bulletedText: [String], details: String = "") {
+      self.title = title
+      self.bulletedText = bulletedText
+      self.details = details
+    }
+  }
+}
+
 struct ArticleTextViewModel {
   let title: String
   let details: String
+  let bullet: Bullet?
 
-  init(title: String, details: String) {
+  init(title: String, details: String, bullet: Bullet? = nil) {
     self.title = title
     self.details = details
+    self.bullet = bullet
   }
 }
